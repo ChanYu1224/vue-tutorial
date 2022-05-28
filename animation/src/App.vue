cef238bc02e5243b7dd1005260293e32e66bc424
ChanYu1224 <kinkitunekin@gmail.com>
2022-05-28 11:47:21 +0900

    animationを追加

---
+++ animation/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <button @click="show = !show">切替</button>
+    <p v-if="show">HelloWorld!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      show: true,
+    }
+  }
+}
+</script>
+
+<style>
+.main{
+  width: 70%;
+  margin: auto;
+  padding-top: 5rem;
+}
+</style>
